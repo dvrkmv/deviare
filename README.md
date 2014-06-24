4709778f62b55eb3889c807466c0f698cba4ab63
localpcguy <mike@local-pc-guy.com>
2014-06-23 22:05:43 -0400

    setup gulp build script

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 === Deviare ===
 
 Todo:
--Check if short URL exists before assigning it
--Check if long URL exists and if so, don't assign it a new URL
 -Gulpfile
+-Check if short URL exists before assigning it
+-Check if long URL exists and if so, don't assign it a new URL (? maybe, tracking?)
+-Setup click count
 
--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,76 @@
-var gulp = require('gulp');
+'use strict';
+var gulp = require('gulp'),
+    sass = require('gulp-ruby-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifycss = require('gulp-minify-css'),
+    jshint = require('gulp-jshint'),
+    ngmin = require('gulp-ngmin'),
+    uglify = require('gulp-uglify'),
+    imagemin = require('gulp-imagemin'),
+    rename = require('gulp-rename'),
+    clean = require('gulp-clean'),
+    concat = require('gulp-concat'),
+    cache = require('gulp-cache'),
+    notify = require('gulp-notify'),
+    livereload = require('gulp-livereload');
+
+gulp.task('styles', function() {
+  return gulp.src('public/scss/main.scss')
+    .pipe(sass({ style: 'expanded' }))
+    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+    .pipe(gulp.dest('dist/assets/css'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(minifycss())
+    .pipe(gulp.dest('dist/assets/css'))
+    .pipe(notify({ message: 'Styles task complete' }));
+});
+
+gulp.task('scripts', function() {
+  return gulp.src('public/js/**/*.js')
+    .pipe(jshint('.jshintrc'))
+    .pipe(jshint.reporter('default'))
+    .pipe(ngmin({dynamic: true}))
+    .pipe(concat('core.js'))
+    .pipe(gulp.dest('dist/assets/js'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/assets/js'))
+    .pipe(notify({ message: 'Scripts task complete' }));
+});
+
+gulp.task('images', function() {
+  return gulp.src('public/img/**/*')
+    .pipe(cache(imagemin({ optimizationLevel: 5, progressive: true, interlaced: true })))
+    .pipe(gulp.dest('dist/assets/img'))
+    .pipe(notify({ message: 'Images task complete' }));
+});
+
+gulp.task('clean', function() {
+  return gulp.src(['dist/assets/css', 'dist/assets/js', 'dist/assets/img'], {read: false})
+    .pipe(clean());
+});
+
+gulp.task('default', ['clean'], function() {
+    gulp.start('styles', 'scripts', 'images');
+});
+
+gulp.task('watch', function() {
+
+  // Create LiveReload server
+  var server = livereload();
+
+  // Watch any files in dist/, reload on change
+  gulp.watch(['dist/**']).on('change', function(file) {
+    server.changed(file.path);
+  });
+
+  // Watch .scss files
+  gulp.watch('src/styles/**/*.scss', ['styles']);
+
+  // Watch .js files
+  gulp.watch('src/scripts/**/*.js', ['scripts']);
+
+  // Watch image files
+  gulp.watch('src/images/**/*', ['images']);
+
+});
