8a208bfa6645e9328ac1ed519ebb9268ea2c4632
localpcguy <mike@local-pc-guy.com>
2014-06-26 23:14:56 -0400

    update routes to redirect / to local-pc-guy.com still

--- app/routes.js
+++ app/routes.js
@@ -125,6 +125,10 @@ module.exports = function(app) {
 
 	// application -------------------------------------------------------------
 	app.get('/', function(req, res) {
+		res.setHeader('Location', 'http://www.local-pc-guy');
+	});
+
+	app.get('/add', function(req, res) {
 		res.sendfile('../public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
 	});
 
