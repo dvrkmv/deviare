70da00fc5a3ba724fa104d81993529a9d6c894f5
localpcguy <mike@local-pc-guy.com>
2014-08-31 01:08:48 -0400

    add example apivars file, without sensitive data

---
+++ app/apivars_EXAMPLE.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports = {
+	'tw_consumerKey'	:	'TWITTER_CONSUMER_KEY_HERE',
+	'tw_consumerSecret'	:	'TWITTER_CONSUMER_SECRET_HERE',
+	'tw_authUrl'		:	'LOCAL_AUTH_URL', // example: '/auth/twitter'
+	'tw_callbackUrl'	:	'FULLY_QUALIFIED_CALLBACK_URL_HERE'  // example: 'http://lpg.io/auth/twitter/callback'
+};
